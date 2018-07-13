d1d5688b586070c8f5314825d3e52b88590a2a41
3apy6a <zaruba.av@gmail.com>
2018-07-13 21:15:01 +0300

    Third assignement

--- minutes_decade.rb
+++ minutes_decade.rb
@@ -1,5 +1,5 @@
-p " Task two: how many minutes in a decade?"
-DEKADE = 10  #namber of days in a decade
+p "Task two: how many minutes in a decade?"
+DEKADE = 10 #number of days in a decade
 DAY = 24 #number of hours in a day
-HOUR = 60 #namber of minute in a hour
+HOUR = 60 #number of minute in a hour
 p (DEKADE*DAY)*HOUR
