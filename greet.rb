8e4a8c3e9765620c9c35713c8cff6a341ce8d930
3apy6a <zaruba.av@gmail.com>
2018-07-16 16:43:13 +0300

    task Two

---
+++ greet.rb
@@ -0,0 +1,5 @@
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'and at the end what\'s your surname?'
+sName = gets.chomp
+puts 'My greetings ' + name + ' ' + sName + ' you won in AZINO 777!'
--- sec_life.rb
+++ sec_life.rb
@@ -1,8 +1,8 @@
 p "Task two: how many seconds in my life?"
 YEAR = 365 #number of days in a year
-OLD = 32 #number of lived years
+OLD = gets.to_i #number of lived years
 DAY = 24 #number of hours in a day
 HOUR = 60 #number of minute in a hour
 MIN = 60 #number of second in a minute
 V_DAYS = OLD/4 # a leap year
-p ((YEAR*OLD)+V_DAYS)*DAY*HOUR*MIN
+p ((YEAR * OLD) + V_DAYS) * DAY * HOUR * MIN
