3d1e8cb618e48d46d6c3652aadbe5502f27aea30
3apy6a <zaruba.av@gmail.com>
2018-07-16 20:10:40 +0300

    task two assignement with numbers& strings

--- sec_life.rb
+++ sec_life.rb
@@ -1,8 +1,10 @@
-p "Task two: how many seconds in my life?"
+puts "Task two: how many seconds in my life?"
+puts 'How old are you?'
 YEAR = 365 #number of days in a year
 OLD = gets.to_i #number of lived years
 DAY = 24 #number of hours in a day
 HOUR = 60 #number of minute in a hour
 MIN = 60 #number of second in a minute
 V_DAYS = OLD/4 # a leap year
-p ((YEAR * OLD) + V_DAYS) * DAY * HOUR * MIN
+puts 'The answer is:'
+p ((YEAR * OLD) + V_DAYS) * DAY * HOUR * MIN    
