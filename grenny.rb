31e6de2e326143734671e9b67f2eb93cd50944f7
3apy6a <zaruba.av@gmail.com>
2018-07-18 17:45:34 +0300

    майже все

--- grenny.rb
+++ grenny.rb
@@ -9,11 +9,11 @@ puts 'WHO IS THERE?'
 puts 'GRANDSON? IT IS YOU?'
 talking = ''
 while talking != 'BYE'
-    puts 'HUH?!  SPEAK UP, SONNY!'
     talking = gets.chomp
     if talking == talking.upcase
         puts "NO, NOT SINCE #{rand(1938..1950)}"
-        talking = gets.chomp
+    else
+        puts "HUH?! SPEAK UP, SONNY!"    
     end
 end
 puts 'BYE GRANDSANY, COME AGAIN'
